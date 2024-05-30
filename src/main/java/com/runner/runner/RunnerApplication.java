c6abf3591dea6329a177cd0f45b55c6e91df7f56
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-05-30 12:33:25 +0530

    Project initialized

---
+++ src/main/java/com/runner/runner/RunnerApplication.java
@@ -0,0 +1,14 @@
+package com.runner.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerApplication.class, args);
+		System.out.println("Application Working!!!");
+    }
+
+}
